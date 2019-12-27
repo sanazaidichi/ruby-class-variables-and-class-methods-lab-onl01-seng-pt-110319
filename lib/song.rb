fc854bfb2030f060ace8eba79b8f3d5f56de904d
Sana Zaidi <sana.rizvi.5@gmail.com>
2019-12-27 06:26:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,20 +1,32 @@
 class Song
   attr_accessor :name, :artist, :genre 
   
-  @@count = 0 
+  @@count = 0
+  @@artist = []
+  @@genres = [] 
   
   
   def initialize(song_name, artist, genre)
     @@count += 1 
     @name = song_name 
-    @genres = []
-    @@artist = []
+    @artist = artist 
+    @genre = genre 
+    @@genres << genre
+    @@artists << artist
   end  
     
     def self.count
       @@count
     end 
     
-    def self.genres 
-    
+    def self.genres
+    @@genres.uniq!
+  end
+
+  def self.artists
+    @@artists.uniq!
+  end
+  
+  def self.genres_count 
+  end 
 end 
